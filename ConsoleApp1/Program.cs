57bc752ea68e85c397da3edb719d5fbff731bb6b
Reesha Saeed <reesha.saeed@elev.ga.ntig.se>
2026-01-03 16:22:16 +0100

    ..

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,44 @@
 ﻿
 class program
 {
+      static List<string> säck = new List<string>();
+
+      static bool gamecontinue= true;
     static void Main()
     {
         Console.WriteLine("=== TEXTÄVENTYRET ===");
+
+        while (gamecontinue)
+        {
+            StartScene();
+            Console.WriteLine("Vill du spela igen? (Ja/Nej). ");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer!= "ja")
+            {
+                gamecontinue = false;
+            }
+        }
+
+    }
+     
+     static void StartScene()
+    {
         Console.WriteLine("\nDu vaknar upp i en mörk skog.");
         Console.WriteLine("Vänster mot floden.");
         Console.WriteLine("Höger mot berget.");
         Console.Write("Vad väljer du?");
         string choice = Console.ReadLine().ToLower();
         if (choice == "vänster")
-            River();         
+            RiverScene();         
         else if (choice == "höger")
-            Mountain();
+            MountainScene();
         else
          Console.WriteLine("Du irrade bort dig i skogen.");
-
+        
     }
-    
-    static void River()
+     
+    static void RiverScene()
     {
 
         Console.WriteLine("\nDu når floden och ser en gammal bro.");
@@ -27,15 +47,65 @@ class program
 
         if (choice == "ja")
         {
-            Console.WriteLine("Du hittar en silvernyckel!");
+            Console.WriteLine("Du hittar en Silvernyckel!");
+            säck.Add("Silvernyckel");
         }
         else
         {
-            
+           Console.WriteLine("Stigen försvann bakom dig ");
         }
 
     }
+
+
+     static void MountainScene()
+    {
+        Console.WriteLine("\nDu når berget och ser en grotta.");
+        Console.Write("Gå in i grottan? (ja/nej) ");
+        string choice = Console.ReadLine().ToLower();
+
+        if (choice == "ja")
+        {
+            Console.WriteLine("Väktaren säger: Sök sanningen i floden.");
+            RiverScene();
+        }
+        else
+        {
+           Console.Writeline("stigen försvann bakom dig och du dör");  
+        }
     
+    }        
+    
+static void Ending()
+    {
+        Console.WriteLine("\n--- SLUTET ---");
+        bool Ending = true;
+        
+        if ("Ending == true && säck.contain== Silvernyckel")
+        {
+            Console.WriteLine("Du fann friheten.");
+            Console.WriteLine("Gratis");
+        }
+        
+        else
+        {
+            Console.WriteLine("Du överlevde, men saknade modet.");
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
